d9bdfa859cc588b478590db22f8fc7e98f28d594
Lil Erik <dmb950311@gmail.com>
2023-01-26 00:37:03 +1100

    firebase setup

---
+++ src/Firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgYtqqJFbQxk0wtzL-2aDODxwT712LOwM",
+  authDomain: "empty-fridge-4cad0.firebaseapp.com",
+  projectId: "empty-fridge-4cad0",
+  storageBucket: "empty-fridge-4cad0.appspot.com",
+  messagingSenderId: "750218833559",
+  appId: "1:750218833559:web:b25cb39e6615aeb64703d2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
